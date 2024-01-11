e79cc24d04c40ef392c50aabced6fbcb34f5355c
a.Lyakhov <a.lyakhov@intellectika.ru>
2024-01-11 17:49:24 +0300

    Добавил UI на activity_shop_item.xml

---
+++ app/src/main/java/com/wexil/shoplist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.wexil.shoplist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wexil.shoplist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
